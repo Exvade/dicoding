351e850e2549dfcc827824fd3d3648ab91746a85
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-10-07 14:33:15 +0700

    -feat adding delete handling

--- contacts-app/contacts-app/src/ContactItem.jsx
+++ contacts-app/contacts-app/src/ContactItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ContactItemBody from './ContactItemBody';
 import ContacItemImage from './ContactItemImage';
+import DeleteButton from './DeleteButton';
 
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({ imageUrl, name, tag, id, onDelete }) {
     return (
         <div className="contact-item">
             <ContacItemImage imageUrl={imageUrl} />
             <ContactItemBody name={name} tag={tag} />
+            <DeleteButton id={id} onDelete={onDelete} />
         </div>
     );
 }
--- stateful-component/latihan-stateful-component/src/main.jsx
+++ stateful-component/latihan-stateful-component/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+class MyComponent extends React.Component {
+  render() {
+    return <p>Hello, class component!</p>;
+  }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<MyComponent />);
